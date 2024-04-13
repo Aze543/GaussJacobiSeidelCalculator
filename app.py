0a4c7e228a5a3d92736a78a13d99b83b890b1547
eizen inopia <eizeninopia@eizens-MacBook-Pro.local>
2024-04-14 00:52:32 +0800

    patch2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from jacopyseidel import JacopySeidel as js
-        
+
 def main():
     on = True
     unwanted_item = [" ", "+"]
@@ -8,22 +8,27 @@ def main():
         equation1 = [item for item in [*input("equation1(x): ")] if item not in unwanted_item]
         equation2 = [item for item in [*input("equation2(y): ")] if item not in unwanted_item]
         equation3 = [item for item in [*input("equation3(z): ")] if item not in unwanted_item]
-        matrix = js([equation1, equation2, equation3])
-        matrix.view()
-        result = matrix.test_diagonally_dominant()
-        print(result)
-        if matrix.compare:
-            matrix.method_comparison()
-        response = input("\nwould you like to go again? Y or N: ").upper()
-        if response == "Y":
+        try:
+            matrix = js([equation1, equation2, equation3])
+        except Exception:
+            print("\n[ERROR] Invalid input.")
             continue
-        elif response == "N":
-            on = False
         else:
-            print("invalid input")
-            on = False
-    print("Program closed")
-           
+            matrix.view()
+            result = matrix.test_diagonally_dominant()
+            print(result)
+            if matrix.compare:
+                matrix.method_comparison()
+            response = input("\nwould you like to go again? Y or N: ").upper()
+            if response == "Y":
+                continue
+            elif response == "N":
+                on = False
+            else:
+                print("\ninvalid input")
+                on = False
+        print("\nProgram closed\n")
+            
             
 if __name__ == "__main__":
     main()
--- jacopyseidel.py
+++ jacopyseidel.py
@@ -1,14 +1,19 @@
+import pandas as pd
+import tabulate as tbl
+
 class JacopySeidel:
     def __init__(self, matrix) -> None:
         """
         constructs the matrix.
         """
-        self._matrix = []
-        self.jacopy_counter = 0
-        self.seidel_counter = 0
+        self.__matrix = []
+        self.__jacopy_counter = 0
+        self.__seidel_counter = 0
+        self.__use_jacopy = False
+        self.__use_seidel = False
         self.compare = False
-        self.use_jacopy = False
-        self.use_seidel = False
+        self.__gjdata = [[], [], [], []]
+        self.__gsdata = [[], [], [], []]
         for array in matrix:
             right_side = ""
             sign_collector = ""
@@ -36,7 +41,7 @@ class JacopySeidel:
                     sign_collector = ""
                     n_before_variable = f"{n}"
             arr.append(int(right_side))
-            self._matrix.append(arr)
+            self.__matrix.append(arr)
             
             
     def view(self) -> None:
@@ -46,7 +51,7 @@ class JacopySeidel:
         print("\nyour equation convert into matrix:")
         n = 0
         letters = ["x", "y", "z"]
-        for arr in self._matrix:
+        for arr in self.__matrix:
             print(f"{arr[:-1]} [{letters[n]}] = {[arr[3]]}")
             n +=1
     def test_diagonally_dominant(self) -> None:
@@ -56,7 +61,7 @@ class JacopySeidel:
         print("\ntesting if the matrix is diagonally dominant.")
         diagonal_item = 0
         counter = 0
-        for arr in self._matrix:
+        for arr in self.__matrix:
             arr = arr[:-1]
             sum = 0
             for item in arr:
@@ -69,89 +74,160 @@ class JacopySeidel:
         if counter == 3:
             on = True
             while on:
-                response = input("\nthe matrix is diagonally dominant, what method would you like to use?\ntype (GJ) for gauss-jacopy and (GS) for gauss-seidel\n").upper()
+                response = input("\nthe matrix is diagonally dominant, what method would you like to __use?\ntype (GJ) for gauss-jacopy and (GS) for gauss-seidel\n").upper()
                 if response == "GJ":
                     on = False
                     self.compare = True
-                    self.use_seidel = True
-                    return self._gauss_jacopy()
+                    self.__use_seidel = True
+                    return self.__gauss_jacopy()
                 elif response == "GS":
                     on = False
                     self.compare = True
-                    self.use_jacopy = True
-                    return self._gauss_seidel()
+                    self.__use_jacopy = True
+                    return self.__gauss_seidel()
                 else:
                     print("invalid input.")
         else:
-            print("\nthe matrix is not diagonally dominant, the program will use Gauss-Seidel.")
-            return self._gauss_seidel()
+            print("\nthe matrix is not diagonally dominant, the program will __use Gauss-Seidel.")
+            return self.__gauss_seidel()
              
              
-    def _gauss_seidel(self) -> str:
+    def __gauss_seidel(self) -> str:
         """
-        performs gauss-seidel method
+        performs Gauss-Seidel method
         """
-        if not self.use_seidel:
+        if not self.__use_seidel:
             print("\nusing Gauss-Seidel...")
         x, y, z = 0, 0, 0
-        arr_x  = self._matrix[0]
-        arr_y = self._matrix[1]
-        arr_z = self._matrix[2]
+        arr_x  = self.__matrix[0]
+        arr_y = self.__matrix[1]
+        arr_z = self.__matrix[2]
         sx, sy, sz = 0, 0, 0
         while True:
+            self.__write_to_table(data=self.__gsdata, index=self.__seidel_counter, values=[x, y, z])
             new_x = round((arr_x[3] - (arr_x[1] * y) - (arr_x[2] * z))/arr_x[0], 3)
             x = new_x
             new_y = round((arr_y[3] - (arr_y[0] * x) - (arr_y[2] * z))/arr_y[1], 3)
             y = new_y
             new_z = round((arr_z[3] - (arr_z[0] * x) - (arr_z[1] * y))/arr_z[2], 3)
             z = new_z
-            self.seidel_counter += 1
-            if self.seidel_counter > 100:
-                return "\nerror: this system of linear equation doesn't converge."
+            if self.__seidel_counter > 100:
+                return f"\nthis system of linear equation doesn't or takes time to converge, the program will use Cramer's Rule instead.\n{self.__cramers_rule()}"
             if (sx, sy, sz) == (x, y, z):
                 break
             else:
+                self.__seidel_counter += 1
                 sx, sy, sz = x, y, z
-        if self.use_jacopy:
-            self._gauss_jacopy()
-        return f"\nResult: x = {x}, y = {y}, z = {z}\nnumber of iterations: {self.seidel_counter}"
+        if self.__use_jacopy:
+            self.__gauss_jacopy()
+        return f"\nResult: x = {x}, y = {y}, z = {z}\nnumber of iterations: {self.__seidel_counter+1}\n{self.__show_iteration_table(data=self.__gsdata)}"
             
 
-    def _gauss_jacopy(self) -> str:
+    def __gauss_jacopy(self) -> str:
         """
-        performs gauss-jacopy method
+        Performs Gauss-Jacopy method
         """
-        if not self.use_jacopy:
+        if not self.__use_jacopy:
             print("\nusing Gauss-Jacopy...")
         x, y, z = 0, 0, 0
-        arr_x  = self._matrix[0]
-        arr_y = self._matrix[1]
-        arr_z = self._matrix[2]
+        arr_x  = self.__matrix[0]
+        arr_y = self.__matrix[1]
+        arr_z = self.__matrix[2]
         while True:
+            self.__write_to_table(data=self.__gjdata, index=self.__jacopy_counter, values=[x, y, z])
             new_x = round((arr_x[3] - (arr_x[1] * y) - (arr_x[2] * z))/arr_x[0], 3)
             new_y = round((arr_y[3] - (arr_y[0] * x) - (arr_y[2] * z))/arr_y[1], 3)
             new_z = round((arr_z[3] - (arr_z[0] * x) - (arr_z[1] * y))/arr_z[2], 3)
-            self.jacopy_counter += 1
-            if self.jacopy_counter > 100:
-                return "\nerror: this system of linear equation doesn't converge."
+            if self.__jacopy_counter > 100:
+                return f"\nthis system of linear equation doesn't or takes time to converge, the program will use Cramer's Rule instead.\n{self.__cramers_rule()}"
             if (new_x, new_y, new_z) == (x, y, z):
                 break
             else:
+                self.__jacopy_counter += 1
                 x, y, z = new_x, new_y, new_z
         
-        if self.use_seidel:
-            self._gauss_seidel()
-        return f"\nResult: x = {x}, y = {y}, z = {z}\nnumber of iterations: {self.jacopy_counter}"
+        if self.__use_seidel:
+            self.__gauss_seidel()
+        return f"\nResult: x = {x}, y = {y}, z = {z}\nnumber of iterations: {self.__jacopy_counter+1}\n{self.__show_iteration_table(data=self.__gjdata)}"
+    
+    def __cramers_rule(self):
+        """
+        performs Cramer's Rule
+        """
+        denominator = self.__determinant(self.__matrix)
+
+        numerator_x = self.__determinant([
+            [self.__matrix[0][3], self.__matrix[0][1], self.__matrix[0][2]],
+            [self.__matrix[1][3], self.__matrix[1][1], self.__matrix[1][2]],
+            [self.__matrix[2][3], self.__matrix[2][1], self.__matrix[2][2]]
+            ])
+        
+        numerator_y = self.__determinant([
+            [self.__matrix[0][0], self.__matrix[0][3], self.__matrix[0][2]],
+            [self.__matrix[1][0], self.__matrix[1][3], self.__matrix[1][2]],
+            [self.__matrix[2][0], self.__matrix[2][3], self.__matrix[2][2]]
+            ])
+
+        numerator_z = self.__determinant([
+            [self.__matrix[0][0], self.__matrix[0][1], self.__matrix[0][3]],
+            [self.__matrix[1][0], self.__matrix[1][1], self.__matrix[1][3]],
+            [self.__matrix[2][0], self.__matrix[2][1], self.__matrix[2][3]]
+            ])
+        
+        x = round(numerator_x/denominator, 3)
+        y = round(numerator_y/denominator, 3)
+        z = round(numerator_z/denominator, 3)
+    
+        return f"\nx = {x}, y = {y}, z = {z}"
+    
+    def __determinant(self, matrix):
+        result = (
+            (matrix[0][0]*((matrix[1][1]*matrix[2][2])-(matrix[2][1]*matrix[1][2]))) 
+            - (matrix[0][1]*((matrix[1][0]*matrix[2][2])-(matrix[2][0]*matrix[1][2]))) 
+            + (matrix[0][2]*((matrix[1][0]*matrix[2][1])-(matrix[2][0]*matrix[1][1])))
+            )
+        return result
+    
     
     def method_comparison(self):
         """
         compares the two methods.
         """
-        if self.jacopy_counter < self.seidel_counter:
+        if self.__jacopy_counter < self.__seidel_counter:
             print("comparison: The Gauss-Jacopy method is more efficient than the Gauss-Seidel method.")
-        elif self.seidel_counter < self.jacopy_counter:
+            response = input("Would you like to see the other table? Y or N: ").upper()
+            if response == "Y":
+                if self.__use_jacopy == True:
+                    print(f"\niterations: {self.__jacopy_counter+1}")
+                    print(self.__show_iteration_table(self.__gjdata))
+                elif self.__use_seidel == True:
+                    print(f"\niterations: {self.__seidel_counter+1}")
+                    print(self.__show_iteration_table(self.__gsdata))
+                    
+        elif self.__seidel_counter < self.__jacopy_counter:
             print("comparison: The Gauss-Seidel method is more efficient than the Gauss-Jacopy method.")
+            response = input("Would you like to see the other table? Y or N: ").upper()
+            if response == "Y":
+                if self.__use_jacopy == True:
+                    print(f"\niterations: {self.__jacopy_counter+1}")
+                    print(self.__show_iteration_table(self.__gjdata))
+                elif self.__use_seidel == True:
+                    print(f"\niterations: {self.__seidel_counter+1}")
+                    print(self.__show_iteration_table(self.__gsdata))
+                    
         else:
             print("comparison: Both of the methods are efficient for this system of linear equations.")
-        
+            
+
+    def __write_to_table(self, data, index, values):
+        data[0].append(index)
+        data[1].append(values[0])
+        data[2].append(values[1])
+        data[3].append(values[2])
+    
+    def __show_iteration_table(self, data):
+        arr = {"iteration": data[0], "x": data[1], "y": data[2], "z": data[3]}
+        df = pd.DataFrame(arr)
+        table = tbl.tabulate(df, headers='keys', tablefmt="grid", showindex=False)
+        return table
             
