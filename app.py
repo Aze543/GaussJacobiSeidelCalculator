29c5aeaa203d49fe343569d45dd3584f00a53e75
Aze543 <eizen.inopia.9@gmail.com>
2024-05-09 02:30:47 +0800

    fix update

--- app.py
+++ app.py
@@ -4,23 +4,18 @@ def main():
     unwanted_item = [" ", "+"]
     while True:
         print("\nGauss-Jacopy, Gauss-Seidel, and Relaxation calculator!\n")
-        equation1 = [
-            item for item in [*input("equation1: ")] if item not in unwanted_item
-        ]
-        equation2 = [
-            item for item in [*input("equation2: ")] if item not in unwanted_item
-        ]
-        equation3 = [
-            item for item in [*input("equation3: ")] if item not in unwanted_item
-        ]
+        equation1 = [item for item in [*input("equation1: ")] if item not in unwanted_item]
+        equation2 = [item for item in [*input("equation2: ")] if item not in unwanted_item]
+        equation3 = [item for item in [*input("equation3: ")] if item not in unwanted_item]
         try:
             matrix = Calculator(matrix=[equation1, equation2, equation3])
-        except Exception:
-            print(f"\n[ERROR] Invalid input.")
+        except Exception as e:
+            print(f"\n[ERROR: {e}] Invalid input.")
             continue
         else:
             print("\ntesting if the matrix is diagonally dominant.")
             result = matrix.test_diagonal_dominance()
+            
             if result:
                 matrix.view_matrix()
                 relaxation = matrix.relaxation()
@@ -32,11 +27,11 @@ def main():
                     print("\n(GJ) for Gauss-Jacopy Method\n(GS) for Gauss-Seidel Method\n(RS) for Relaxation Method\n(C) for comparison\n(E) to go back")
                     response = input("\ninput: ").upper()
                     if response == "GJ":
-                        print(f"\nGauss-Jacopy Method\nnumber of iterations: {jacopy[0]}\n{jacopy[1]}\nResult: x = {jacopy[2]}, y = {jacopy[3]}, z = {jacopy[4]}\n")
+                        print(f"\nGauss-Jacopy Method\n{jacopy[5]}\nnumber of iterations: {jacopy[0]}\n{jacopy[1]}\nResult: x = {jacopy[2]}, y = {jacopy[3]}, z = {jacopy[4]}\n\nelapsed time: {jacopy[6]}\n")
                     elif response == "GS":
-                        print(f"\nGauss-Seidel Method\nnumber of iterations: {seidel[0]}\n{seidel[1]}\nResult: x = {seidel[2]}, y = {seidel[3]}, z = {seidel[4]}\n")
+                        print(f"\n\nGauss-Seidel Method\n{seidel[5]}\nnumber of iterations: {seidel[0]}\n{seidel[1]}\nResult: x = {seidel[2]}, y = {seidel[3]}, z = {seidel[4]}\n\nelapsed time: {seidel[6]}\n")
                     elif response == "RS":
-                        print(f"\nRelaxation Method\nnumber of iterations: {relaxation[0]}\n{relaxation[1]}\nTOTAL: x = {relaxation[2]}, y = {relaxation[3]}, z = {relaxation[4]}\n")
+                        print(f"\nRelaxation Method\n{relaxation[5]}\nnumber of iterations: {relaxation[0]}\n{relaxation[1]}\nTOTAL: x = {relaxation[2]}, y = {relaxation[3]}, z = {relaxation[4]}\n\nelapsed time: {relaxation[6]}\n")
                     elif response == "C":
                         matrix.view_ranking()
                     elif response == "E":
@@ -45,12 +40,12 @@ def main():
                         print("[ERROR] invalid input.")  
             else:
                 matrix.view_matrix()
-                print(
-                    "\nthe matrix is not diagonally dominant, the program will use Cramers-Rule."
-                )
+                print("\nthe matrix is not diagonally dominant, the program will use Gauss Seidel and Cramers-Rule.")
+                seidel = matrix.gauss_seidel()
+                print(f"\n\nGauss-Seidel Method\n{seidel[5]}\nnumber of iterations: {seidel[0]}\n{seidel[1]}\nResult: x = {seidel[2]}, y = {seidel[3]}, z = {seidel[4]}\n\nelapsed time: {seidel[6]}\n")
                 cr = matrix.cramers_rule()
                 if type(cr) != bool:
-                    print(f"\nCramers-Rule\nResult: x = {cr[0]}, y = {cr[1]}, z = {cr[2]}\n")
+                    print(f"\nCramer's Rule\nResult: x = {cr[0]}, y = {cr[1]}, z = {cr[2]}\n")
             
             response = input("\nwould you like to go again? Y or N: ").upper()
             if response == "Y":
--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tabulate as tbl
+import time
 from numpy import argsort
 
 
@@ -12,47 +13,37 @@ class Calculator:
         self.__jacopy_counter = 0
         self.__seidel_counter = 0
         self.__relaxation_counter = 0
+        self.__gj_approx = ""
+        self.__gs_approx = ""
+        self.__rs_approx = ""
         self.__gjdata = [[], [], [], []]
         self.__gsdata = [[], [], [], []]
         self.__rsdata = [[], [], [], [], [],[], [], []]
-        #
         for array in matrix:
-            right_side = ""
-            sign_collector = ""
-            n_before_variable = ""
             arr = []
-            for n in array:
-                if n == "-":
-                    sign_collector = n
-                    continue
-                elif n == "=":
-                    arr_i = array.index(n)
-                    for n in range(arr_i + 1, len(array)):
-                        right_side += str(array[n])
-                    break
+            n, sign = "", ""
+            for item in array:
                 try:
-                    float(n)
+                    float(item)
                 except Exception:
-                    if n_before_variable == "":
-                        arr.append(int(f"{sign_collector}{1}"))
-                        sign_collector = ""
+                    if item == ".":
+                        n += item
+                    elif item == "-":
+                        sign += item
+                    elif item == "=":
+                        for i in range(array.index(item)+ 1, len(array)):
+                            n += str(array[i])
+                        arr.append(float(sign+n))
+                        break
                     else:
-                        n_before_variable = ""
+                        if n == "":
+                            n += "1"
+                        arr.append(float(sign+n))
+                        n, sign = "", ""
                 else:
-                    arr_i = array.index(n)
-                    for i in range(arr_i + 1, len(array)):
-                        try:
-                            float(array[i])
-                        except Exception:
-                            break
-                        else:
-                            n = str(n) + str(array[i])
-                            array.remove(array[i])
-
-                    arr.append(float(f"{sign_collector}{float(n)}"))
-                    sign_collector = ""
-                    n_before_variable = f"{n}"
-            arr.append(float(right_side))
+                    n += item
+            if len(arr[:-1]) != 3:
+                float("x")
             self.__matrix.append(arr)
 
     def view_matrix(self) -> None:
@@ -80,14 +71,14 @@ class Calculator:
                 arr = arr[:-1]
                 sum = 0
                 for item in arr:
-                    if arr[diagonal_item] == item:
+                    if arr.index(item) == diagonal_item:
                         continue
                     sum += abs(item)
                 if arr[diagonal_item] > sum:
                     counter += 1
                 diagonal_item += 1
             if counter == 3:
-                self.__matrix= matrix
+                self.__matrix = matrix
                 return True
             elif p_counter == 6:
                 self.__matrix= [matrix[2], matrix[1], matrix[0]]
@@ -101,61 +92,83 @@ class Calculator:
         """
         performs Gauss-Seidel method
         """
+        start = time.time()
         x, y, z = 0, 0, 0
         arr_x = self.__matrix[0]
         arr_y = self.__matrix[1]
         arr_z = self.__matrix[2]
         sx, sy, sz = 0, 0, 0
+        self.__gs_approx += f"\ninitial values: x = {x}, y = {y}, z = {z}\n"
+        self.__gjs_to_table(data=self.__gsdata, index=self.__seidel_counter, values=[sx,sy, sz])
+        self.__seidel_counter += 1
         while True:
+            self.__gs_approx += f"\n>> approximation #{self.__seidel_counter}\n"
             x = round((arr_x[3] - (arr_x[1] * y) - (arr_x[2] * z)) / arr_x[0], 3)
+            self.__gs_approx += f"\nx = ({arr_x[3]} - ({arr_x[1]} * {y}) - ({arr_x[2]} * {z}))/{arr_x[0]} = {x}\n"
+            
             y = round((arr_y[3] - (arr_y[0] * x) - (arr_y[2] * z)) / arr_y[1], 3)
+            self.__gs_approx += f"y = ({arr_x[3]} - ({arr_x[0]} * {y}) - ({arr_x[2]} * {z}))/{arr_x[1]} = {y}\n"
+            
             z = round((arr_z[3] - (arr_z[0] * x) - (arr_z[1] * y)) / arr_z[2], 3)
-            self.__gjs_to_table(
-                data=self.__gsdata, index=self.__seidel_counter, values=[sx,sy, sz]
-            )
+            self.__gs_approx += f"z = ({arr_x[3]} - ({arr_x[0]} * {y}) - ({arr_x[1]} * {z}))/{arr_x[2]} = {z}\n"
+            
+            self.__gjs_to_table(data=self.__gsdata, index=self.__seidel_counter, values=[x,y,z])
             if (sx, sy, sz) == (x, y, z):
+                elapsed_time = f"{round((time.time() - start) * 10**3, 3)} ms"
                 break
+            elif self.__relaxation_counter > 100:
+                return "the matrix is diverginng."
             else:
                 self.__seidel_counter += 1
-                sx, sy, sz = x, y, z
-                
-        return [self.__seidel_counter, self.__jacopy_seidel_table(data=self.__gsdata), x, y, z]
+                sx, sy, sz = x, y, z     
+        return [self.__seidel_counter, self.__jacopy_seidel_table(data=self.__gsdata), x, y, z, self.__gs_approx, elapsed_time]
 
     def gauss_jacopy(self) -> list:
         """
         Performs Gauss-Jacopy method
         """
+        start = time.time()
         x, y, z = 0, 0, 0
         arr_x = self.__matrix[0]
         arr_y = self.__matrix[1]
         arr_z = self.__matrix[2]
+        self.__gj_approx += f"\ninitial values: x = {x}, y = {y}, z = {z}\n"
         while True:
-            self.__gjs_to_table(
-                data=self.__gjdata, index=self.__jacopy_counter, values=[x, y, z]
-            )
+            self.__gjs_to_table(data=self.__gjdata, index=self.__jacopy_counter, values=[x, y, z])
             new_x = round((arr_x[3] - (arr_x[1] * y) - (arr_x[2] * z)) / arr_x[0], 3)
             new_y = round((arr_y[3] - (arr_y[0] * x) - (arr_y[2] * z)) / arr_y[1], 3)
             new_z = round((arr_z[3] - (arr_z[0] * x) - (arr_z[1] * y)) / arr_z[2], 3)
             if (new_x, new_y, new_z) == (x, y, z):
+                elapsed_time = f"{round((time.time() - start) * 10**3, 3)} ms"
                 break
             else:
                 self.__jacopy_counter += 1
+                self.__gj_approx += f"\n>> approximation #{self.__jacopy_counter}\n"
+                self.__gj_approx += f"\nx = ({arr_x[3]} - ({arr_x[1]} * {y}) - ({arr_x[2]} * {z}) / {arr_x[0]} = {new_x}\n"
+                self.__gj_approx += f"y = ({arr_y[3]} - ({arr_y[0]} * {x}) - ({arr_y[2]} * {z}) / {arr_y[1]} = {new_y}\n"
+                self.__gj_approx += f"z = ({arr_z[3]} - ({arr_z[0]} * {x}) - ({arr_z[1]} * {y}) / {arr_z[2]} = {new_z}\n"
                 x, y, z = new_x, new_y, new_z
                 
-        return [self.__jacopy_counter, self.__jacopy_seidel_table(data=self.__gjdata), x, y, z]
+        return [self.__jacopy_counter, self.__jacopy_seidel_table(data=self.__gjdata), x, y, z, self.__gj_approx, elapsed_time]
 
     def relaxation(self) -> list:
         """
         Performs Gauss-Jacopy method
         """
+        start = time.time()
         x,y,z = 0,0,0
         dx,dy,dz = 0,0,0
+        tol = 1e-3
         arr_x = self.__matrix[0].copy()
         arr_y = self.__matrix[1].copy()
         arr_z = self.__matrix[2].copy()
         ri_x = max([abs(n) for n in arr_x[:-1]])
         ri_y = max([abs(n) for n in arr_y[:-1]])
         ri_z = max([abs(n) for n in arr_z[:-1]])
+        arr = {"Residual Increments": ["dx", "dy", "dz"], "R1": [n*-1 for n in arr_x[:-1]], "R2": [n*-1 for n in arr_y[:-1]], "R3": [n*-1 for n in arr_z[:-1]]}
+        df = pd.DataFrame(arr)
+        rst = tbl.tabulate(df, headers="keys", tablefmt="grid", showindex=False)
+        self.__rs_approx += f"\n{rst}\n"
         f_x, f_y, f_z = 0,0,0
         letters = ["x", "y", "z"]
         while True:
@@ -165,29 +178,38 @@ class Calculator:
             arr_x[3] = r1 = round(r1, 4)
             arr_y[3] = r2 = round(r2, 4)
             arr_z[3] = r3 = round(r3, 4)
-            if ([round(abs(n), 2) for n in [r1, r2, r3]]) == ([round(abs(n), 2) for n in [dx, dy, dz]]):
+            operation = 0
+            for n in enumerate([x,y,z]):
+                if abs(n[1]) != 0:
+                    operation = f"d{letters[n[0]]} = {round(n[1], 4)}"
+            self.__rs_to_table(data=self.__rsdata, index=self.__relaxation_counter,operation=operation, dx=x, dy=y, dz=z, r1=r1, r2=r2, r3=r3)
+            self.__relaxation_counter += 1
+            self.__rs_approx += f"\n>> approximation #{self.__relaxation_counter}\n"
+            self.__rs_approx += f"\nR1 = {arr_x[3]} - ({arr_x[0]} * {x}) - ({arr_x[1]} * {y}) - ({arr_x[2]} * {z}) = {r1}\n"
+            self.__rs_approx += f"R2 = {arr_y[3]} - ({arr_y[0]} * {x}) - ({arr_y[1]} * {y}) - ({arr_y[2]} * {z}) = {r2}\n"
+            self.__rs_approx += f"R2 = {arr_y[3]} - ({arr_y[0]} * {x}) - ({arr_y[1]} * {y}) - ({arr_y[2]} * {z}) = {r3}\n"
+            if [abs(n) < tol for n in [r1-dx, r2-dy, r3-dz]] == [True, True, True]:
+                elapsed_time = f"{round((time.time() - start) * 10**3, 3)} ms"
                 break
             else:
-                operation = 0
-                for n in enumerate([x,y,z]):
-                    if abs(n[1]) != 0:
-                        operation = f"d{letters[n[0]]} = {round(n[1], 4)}"
-                self.__rs_to_table(data=self.__rsdata, index=self.__relaxation_counter,operation=operation, dx=x, dy=y, dz=z, r1=r1, r2=r2, r3=r3)
                 if abs(r1) == max([abs(n) for n in [r1, r2, r3]]):
                     dx = round(r1/ri_x, 4)
+                    self.__rs_approx += f"\nmaximum: r1 = {r1}\ndx = {r1}/{ri_x} = {dx}\n"
                     f_x += dx
                     x, y, z = dx,0,0
                 elif abs(r2) == max([abs(n) for n in [r1, r2, r3]]):
                     dy = round(r2/ri_y, 4)
+                    self.__rs_approx += f"\nmaximum: r2 = {r2}\ndy = {r2}/{ri_y} = {dy}\n"
                     f_y += dy
                     x, y, z = 0,dy,0
                 elif abs(r3) == max([abs(n) for n in [r1, r2, r3]]):
                     dz = round(r3/ri_z, 4)
+                    self.__rs_approx += f"\nmaximum: r3 = {r3}\ndx = {r3}/{ri_z} = {dz}\n"
                     f_z += dz
                     x, y, z = 0,0,dz
-                self.__relaxation_counter += 1
+                
             
-        return [self.__relaxation_counter, self.__relaxation_table(data=self.__rsdata), round(f_x, 3),round(f_y, 3),round(f_z, 3)]
+        return [self.__relaxation_counter, self.__relaxation_table(data=self.__rsdata), round(f_x, 3),round(f_y, 3),round(f_z, 3), self.__rs_approx, elapsed_time]
 
     def cramers_rule(self) -> list:
         """
@@ -202,7 +224,6 @@ class Calculator:
                 [self.__matrix[2][3], self.__matrix[2][1], self.__matrix[2][2]],
             ]
         )
-
         numerator_y = self.__determinant(
             [
                 [self.__matrix[0][0], self.__matrix[0][3], self.__matrix[0][2]],
@@ -210,7 +231,6 @@ class Calculator:
                 [self.__matrix[2][0], self.__matrix[2][3], self.__matrix[2][2]],
             ]
         )
-
         numerator_z = self.__determinant(
             [
                 [self.__matrix[0][0], self.__matrix[0][1], self.__matrix[0][3]],
@@ -223,7 +243,7 @@ class Calculator:
             y = round(numerator_y / denominator, 3)
             z = round(numerator_z / denominator, 3)
         except Exception as e:
-            print(f"\n[ERROR: {e}] the operation cannot continue.")
+            print(f"\nCramer's Rule\n[ERROR: {e}] the operation cannot continue.")
             return False
         return [x, y, z]
 
@@ -232,18 +252,9 @@ class Calculator:
         finds the determinant of a matrix.
         """
         result = (
-            (
-                matrix[0][0]
-                * ((matrix[1][1] * matrix[2][2]) - (matrix[2][1] * matrix[1][2]))
-            )
-            - (
-                matrix[0][1]
-                * ((matrix[1][0] * matrix[2][2]) - (matrix[2][0] * matrix[1][2]))
-            )
-            + (
-                matrix[0][2]
-                * ((matrix[1][0] * matrix[2][1]) - (matrix[2][0] * matrix[1][1]))
-            )
+            (matrix[0][0] * ((matrix[1][1] * matrix[2][2]) - (matrix[2][1] * matrix[1][2])))
+            - (matrix[0][1] * ((matrix[1][0] * matrix[2][2]) - (matrix[2][0] * matrix[1][2])))
+            + (matrix[0][2] * ((matrix[1][0] * matrix[2][1]) - (matrix[2][0] * matrix[1][1])))
         )
         return result
 
